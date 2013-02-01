2dd91de34fcef61a691e31d08ef5230583e37e33
Steven Shaw <sps5688@rit.edu>
2013-01-31 22:37:48 -0500

    Added Initial Server
    Began server implementation

--- Secure_IM/src/server/Server.java
+++ Secure_IM/src/server/Server.java
@@ -1,29 +1,43 @@
 package server;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
 
 public class Server {
-
-	private boolean ping(String ipAddress){
-		boolean canReach = true;
-		try {
-			canReach = InetAddress.getByAddress(ipAddress.getBytes()).isReachable(1000);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return canReach;
+	public static ServerPacket packet;
+	public static Socket connection = null;
+	public static ObjectInputStream in;
+	public static ServerSocket socket;
+	
+	public static HashMap<String, ClientInfo> activeUsers = new HashMap<String, ClientInfo>();
+	
+	public static void removeActiveUser(String username){
+		activeUsers.remove(username);
 	}
 	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-
-	}
-
+	public static void main(String[] args){
+	    try{
+	    	socket = new ServerSocket(8010);
+	      
+	    	while(true){
+	    		// Accepts incoming connection
+	    		connection  = socket.accept();
+	    		
+	    		// Retrieves packet
+	    		in = new ObjectInputStream(connection.getInputStream());
+	    		packet = (ServerPacket) in.readObject();
+	    		
+	    		// Add to activeUsers structure
+	    		activeUsers.put(packet.getUsername(), packet.getInfo());
+	    		
+	    		// Services packet
+	    		Thread clientThread = new Thread(new ServerPacket_Handler(packet)); 
+	    		clientThread.start();
+	    	}
+	    }catch(Exception e){
+	    	System.err.print("Gotta catch them all.");
+	    } 
+	  }
 }
